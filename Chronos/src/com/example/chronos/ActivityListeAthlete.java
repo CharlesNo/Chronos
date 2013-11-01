cd39c0fb7773f816666bf39838ac865b43ca3b4a
JeromePoinas <jerome.poinas@yahoo.fr>
2013-11-01 13:18:57 +0100

    Mise a jour ajout d'un athlete et suppression avec longclick mais pb dans l'ajout la liste est tjrs egale a 1

--- Chronos/src/com/example/chronos/ActivityListeAthlete.java
+++ Chronos/src/com/example/chronos/ActivityListeAthlete.java
@@ -2,13 +2,19 @@ package com.example.chronos;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import metier.Athlete;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -26,7 +32,7 @@ import exception.InvalidePrenomException;
 public class ActivityListeAthlete extends Activity
 {
 	/** The mes athletes. */
-	List<Athlete>	mesAthletes;
+	List<Athlete>	mesAthletes = new ArrayList<Athlete>();
 	/** The lv liste. */
 	ListView		lvListe;
 
@@ -43,25 +49,68 @@ public class ActivityListeAthlete extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_activity_chronos);
-		mesAthletes = new ArrayList<Athlete>();
-		try
-		{
-			init();
-			lvListe = (ListView) findViewById(R.id.listAthlete);
-			lvListe.setAdapter(new ArrayAdapter<Athlete>(this,
-					android.R.layout.simple_list_item_1, mesAthletes));
-		}
-		catch (final InvalideNomException e)
-		{
-			Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT)
-					.show();
-		}
-		catch (final InvalidePrenomException e)
-		{
-			Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT)
-					.show();
-		}
+		
+		
+		lvListe = (ListView) findViewById(R.id.listAthlete);
 		final Button addAthlete = (Button) findViewById(R.id.bouttonAddAthlete);
+		/***********************************************************/
+	
+		final ArrayAdapter<Athlete> adapter = new ArrayAdapter<Athlete>(getBaseContext(), android.R.layout.simple_list_item_1,mesAthletes);
+		
+		lvListe.setOnItemClickListener(new OnItemClickListener() {
+				@Override
+	         	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+					//On recupere l'athlete selectionné
+	        		Athlete athleteSelected = (Athlete) lvListe.getItemAtPosition(position);
+	        		//Boite de dialogue
+	        		AlertDialog.Builder adb = new AlertDialog.Builder(ActivityListeAthlete.this);
+	        		adb.setTitle("Sélection Item");
+	        		adb.setMessage("Votre choix : "+athleteSelected.getPrenom()+" "+athleteSelected.getNom());
+	        		adb.setPositiveButton("Ok", null);
+	        		adb.show();
+	        	}
+		});
+		lvListe.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,int position, long id) {
+			        AlertDialog.Builder adb=new AlertDialog.Builder(ActivityListeAthlete.this);
+			        adb.setTitle("Suppression d'un athlete");
+			        adb.setMessage("Etes vous sure de vouloir le supprimer ? ");
+			        final int positionToRemove = position;
+			        adb.setNegativeButton("Cancel", null);
+			        adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+			            public void onClick(DialogInterface dialog, int which) {
+			            	adapter.remove((Athlete) lvListe.getItemAtPosition(positionToRemove));
+			            }});
+			        adb.show();
+			        return true;
+			}});
+		
+
+		//On recupere les données provenant de ActivityAddAthlete si il y en a
+		 Bundle objetbunble  = this.getIntent().getExtras();
+		 
+	        //On récupère les données du Bundle
+	    if (objetbunble != null && objetbunble.containsKey("Nom") && objetbunble.containsKey("Prenom")) {
+	        	String nomAthlete = this.getIntent().getStringExtra("Nom");
+	            String prenomAthlete = this.getIntent().getStringExtra("Prenom");
+	            try {
+	            	Athlete athlete = new Athlete(nomAthlete,prenomAthlete);
+	            	mesAthletes.add(athlete);
+					
+				} catch (InvalideNomException e) {
+					Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT)
+					.show();
+				} catch (InvalidePrenomException e) {
+					Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT)
+					.show();
+				}
+	            
+	    }
+	    lvListe.setAdapter(adapter);
+
+		
+		
 		addAthlete.setOnClickListener(new OnClickListener()
 		{
 			@Override
@@ -72,6 +121,10 @@ public class ActivityListeAthlete extends Activity
 				startActivity(intent);
 			}
 		});
+		
+		
+		
+		
 	}
 
 	/* _________________________________________________________ */
@@ -89,21 +142,5 @@ public class ActivityListeAthlete extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_activity_chronos, menu);
 		return true;
-	}
-
-	/**
-	 * Methode qui initialise la liste des Athletes.
-	 * 
-	 * @throws InvalideNomException
-	 *             Nom null.
-	 * @throws InvalidePrenomException
-	 *             Prenom null.
-	 */
-	private void init() throws InvalideNomException, InvalidePrenomException
-	{
-		mesAthletes.clear();
-		mesAthletes.add(new Athlete("Imbert", "Jean-louis"));
-		mesAthletes.add(new Athlete("Neau", "Charles"));
-		mesAthletes.add(new Athlete("Poinas", "Jerome"));
 	}
 }
